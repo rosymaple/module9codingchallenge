02a18a3d23d0e29e0e30607df557ee1b4c24dc1f
Candace Vogel <156586504+rosymaple@users.noreply.github.com>
2026-03-24 18:10:53 +0000

    commit and push

---
+++ Northwind/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
